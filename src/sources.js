0dbbbb0cf7d7bb8a27e0cd164fb6448abf44ef47
Brad Willard <imbrad91@gmail.com>
2020-06-22 19:21:28 +0200

    Delete sources.js

--- src/sources.js
+++
@@ -1,8 +0,0 @@
-export const BASE_COMPANY_INFO_URL = 'https://financialmodelingprep.com/api/v3/profile/'
-
-export const FINANCIAL_STATEMENT_URL = 'https://financialmodelingprep.com/api/v3/income-statement/'
-
-export const API_KEY = 'e7e39f036c1837577c8c498ec5e10342';
-
-
-
